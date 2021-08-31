c73d64d1fe27306875211eb3f294025186ff848d
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2021-08-31 20:19:04 +1200

    Add runProgram.java
    Creates a program that displays a window in the center of the screen.

---
+++ runProgram.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class runProgram extends JFrame {
+    public runProgram() {
+        super("Java Window Test");
+
+        //Gets screen width and height
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double screenWidth = screenSize.getWidth();
+        double screenHeight = screenSize.getHeight();
+
+        //Sets Java window height
+        int windowWidth = 600;
+        int windowHeight = 400;
+
+        //Exits program upon closing
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Halves screen width and height to find the center of the screen
+        //Then halfs the window size and subtracts it from the halved screen size.
+        setLocation(((int)screenWidth/2)-(windowWidth/2), ((int)screenHeight/2)-(windowHeight/2));         
+        setSize(windowWidth, windowHeight);
+        setVisible(true);
+
+        JLabel label = new JLabel("Hello World", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+
+        JButton close = new JButton("Close");
+        close.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    dispose();
+                    System.exit(0);
+                }
+            }
+        );
+        add(close, BorderLayout.PAGE_END);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new runProgram();
+			}
+		});
+    }
+}
